c8a020d1e4a5e7a69e6bac9aba0be72bbed84402
Rodrigo Vaz <rodrigovaz_sg@hotmail.com>
2020-10-25 16:25:34 -0300

    first commit

---
+++ app/src/main/java/android/example/com/MainActivity.kt
@@ -0,0 +1,50 @@
+package android.example.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView //to improve efficiency of the app, we store this image in this field here. We dont want to use findViewById everytime we use the function rollDice()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll!"
+
+        diceImage = findViewById(R.id.dice_image)
+
+        rollButton.setOnClickListener{
+           //Toast.makeText(this, "The button was clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.text = randomInt.toString()
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+
+
+    }
+}
